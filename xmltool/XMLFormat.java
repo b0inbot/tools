0642f7c3c0a60910959d5e4e48a096c152e88814
boinsoft <me@b0in.xyz>
2025-02-02 00:44:02 -0800

    add rules_lint mode, fixup xmltool for usage

--- xmltool/XMLFormat.java
+++ xmltool/XMLFormat.java
@@ -1,5 +1,6 @@
 package boinsoft.tools.xmltool;
 
+import boinsoft.tools.cli.CLI;
 import boinsoft.tools.cli.SimpleCLI;
 import boinsoft.tools.cli.UsageException;
 import io.vavr.control.Try;
@@ -8,7 +9,9 @@ import java.net.*;
 import java.nio.file.*;
 import java.util.*;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.dom4j.Document;
 import org.dom4j.io.OutputFormat;
@@ -19,12 +22,12 @@ import org.dom4j.io.XMLWriter;
 public class XMLFormat implements SimpleCLI<XMLFormat.CLIOptions> {
 
   public static class CLIOptions {
-    public Path input;
+    public List<Path> inputs;
   }
 
   @Override
   public String usageLine() {
-    return "xmlformat input <flags>";
+    return "xmlformat <flags> input1 input2 ...";
   }
 
   @Override
@@ -55,34 +58,37 @@ public class XMLFormat implements SimpleCLI<XMLFormat.CLIOptions> {
       var files = cl.getArgList();
       if (files.size() == 0) {
         return Try.failure(new UsageException("No files provided"));
-      } else if (files.size() > 1) {
-        return Try.failure(new UsageException("Too many files provided"));
       }
-      var input = Path.of(files.get(0));
+      var inputs = files.stream().map(s -> Path.of(s)).collect(Collectors.toList());
 
       var opts = new CLIOptions();
-      opts.input = input;
+      opts.inputs = inputs;
       return Try.success(opts);
     });
   }
 
   @Override
-  public void options(Options options) {}
+  public void options(Options options) {
+    // added to support in-place utilization with prettier
+    options.addOption(Option.builder("write").longOpt("write").desc("no-op").build());
+  }
 
   @Override
   public void run(CLIOptions cliopts) throws Exception {
     SAXReader reader = new SAXReader();
-    Document document = reader.read(cliopts.input.toFile());
+    for (var input : cliopts.inputs) {
+      Document document = reader.read(input.toFile());
 
-    // write to a string since we are editing inplace. Not the biggest
-    // deal as we have already read it but if the workflow changes or we swap
-    // to java.xml Transformer based design this will be important.
-    StringWriter sw = new StringWriter();
+      // write to a string since we are editing inplace. Not the biggest
+      // deal as we have already read it but if the workflow changes or we swap
+      // to java.xml Transformer based design this will be important.
+      StringWriter sw = new StringWriter();
 
-    formatDocument(document, sw);
+      formatDocument(document, sw);
 
-    try (FileWriter wr = new FileWriter(cliopts.input.toFile())) {
-      wr.write(sw.toString());
+      try (FileWriter wr = new FileWriter(input.toFile())) {
+        wr.write(sw.toString());
+      }
     }
   }
 
@@ -97,5 +103,9 @@ public class XMLFormat implements SimpleCLI<XMLFormat.CLIOptions> {
     XMLWriter writer = new XMLWriter(w, format);
 
     writer.write(document);
+  }
+
+  public static void main(String[] args) throws Exception {
+    CLI.invoke(new XMLFormat(), args);
   }
 }
